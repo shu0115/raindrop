e97d25c7280cad0e9aac09186f3cd9ec3314d22f
shu0115 <s.matsumoto0115@gmail.com>
2013-09-15 16:40:16 +0900

    例外エラーメール送信設定

---
+++ config/initializers/exception_notification.rb
@@ -0,0 +1,8 @@
+unless Rails.env.development?
+  CakesOnWeb::Application.config.middleware.use(
+    ExceptionNotifier,
+    email_prefix:         "[#{Rails.env}][#{Settings.site_name}] ",
+    sender_address:       %{"ExceptionNotifier" <rails.dev0115@gmail.com>},
+    exception_recipients: %w{rails.dev0115@gmail.com},
+  )
+end
