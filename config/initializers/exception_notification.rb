374042d36a35fc4913ffd80267dd9a46e084ed93
shu0115 <s.matsumoto0115@gmail.com>
2013-09-15 16:46:33 +0900

    ExceptionNotification設定修正

--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,8 +1,11 @@
 unless Rails.env.development?
   Raindrop::Application.config.middleware.use(
-    ExceptionNotifier,
-    email_prefix:         "[#{Rails.env}][#{Settings.site_name}] ",
-    sender_address:       %{"ExceptionNotifier" <rails.dev0115@gmail.com>},
-    exception_recipients: %w{rails.dev0115@gmail.com},
+    ExceptionNotification::Rack,
+    email: {
+      email_prefix:         "[#{Rails.env}][#{Settings.site_name}] ",
+      sender_address:       %{"ExceptionNotifier" <rails.dev0115@gmail.com>},
+      exception_recipients: %w{rails.dev0115@gmail.com},
+    }
   )
 end
+
