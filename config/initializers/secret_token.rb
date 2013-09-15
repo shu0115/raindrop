9c551c5707194e155e564e66bcbc7db487e244d6
shu0115 <s.matsumoto0115@gmail.com>
2013-09-15 13:34:09 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raindrop::Application.config.secret_key_base = 'bc3d7d11c14edc778592c9d93900fb9a4e396562589d19b0abfe451bffd52cf965bb72799a2185cd39f90f8ec929dedb4b7eb7c81c42f7ab3a0b188bf468eae3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Raindrop::Application.config.session_store :cookie_store, key: '_raindrop_session'
